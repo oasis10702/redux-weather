0cc233c388290c9ec54d56e02d7bd11eeecc2b3b
oasis10702 <oasis10702@gmail.com>
2018-07-12 23:47:12 +0800

    redux weather example initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '5d4eec68cc60897bc7c5ecb427cd6dd4';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
